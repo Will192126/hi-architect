445799f850353d1283d1e1131ffe5160012e723f
Will Liu <834661621@qq.com>
2020-06-07 18:01:10 +0800

    实现日志文件打印

--- hi-library/app/src/main/java/com/will/hi/library/app/MyApplication.kt
+++ hi-library/app/src/main/java/com/will/hi/library/app/MyApplication.kt
@@ -3,6 +3,7 @@ package com.will.hi.library.app
 import android.app.Application
 import com.alibaba.fastjson.JSON
 import com.will.hi.library.hilibrary.log.HiConsolePrinter
+import com.will.hi.library.hilibrary.log.HiFilePrinter
 import com.will.hi.library.hilibrary.log.HiLogConfig
 import com.will.hi.library.hilibrary.log.HiLogManager
 
@@ -21,6 +22,8 @@ class MyApplication : Application() {
             override fun enable(): Boolean {
                 return true
             }
-        }, HiConsolePrinter())
+        },
+            HiConsolePrinter(),
+        HiFilePrinter.getInstance(getExternalFilesDir(null)!!.canonicalPath + "/hilog", 1000))
     }
 }
