5f1fbb1b525b1033972ef2c214441176cefae30a
Will Liu <834661621@qq.com>
2020-06-29 00:02:34 +0800

    finish tab bottom layout development

--- hi-library/will-library/src/main/java/com/will/library/log/HiLogFormater.java
+++
@@ -1,5 +0,0 @@
-package com.will.library.log;
-
-public interface HiLogFormater<T> {
-    String format(T data);
-}
--- hi-library/will-library/src/main/java/com/will/library/log/HiThreadFormater.java
+++
@@ -1,8 +0,0 @@
-package com.will.library.log;
-
-public class HiThreadFormater implements HiLogFormater<Thread> {
-    @Override
-    public String format(Thread data) {
-        return "Thread:" + data.getName();
-    }
-}
